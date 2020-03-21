e4b67bb9008240c1bbc0c75736e45ede58fbad29
WvM <williamtrans2011@gmail.com>
2020-03-21 05:30:20 +0200

    - Adds app.json

--- src/cache/redisCache.js
+++ src/cache/redisCache.js
@@ -4,9 +4,17 @@ const url = process.env.REDIS_URL || 'redis://localhost:6379';
 
 const client = redis.createClient(url);
 
-client.on("error", (error) => {
-  console.error(error);
-});
+// client.on("error", (error) => {
+//   console.error(error);
+// });
+
+client
+  .on('connect', () => {
+    console.log('redis connected');
+  })
+  .on('error', (error) => {
+    // console.log(error);
+  });
 
 export const setAsync = promisify(client.set).bind(client);
 export const getAsync = promisify(client.get).bind(client);
